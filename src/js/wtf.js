31d2b96cd7ce8818ab9783e57dcfea57252b17d2
Lucas <lucas.silva@agenciamilagro.com.br>
2014-01-23 09:53:43 -0200

    Update wtf.js

--- src/js/wtf.js
+++ src/js/wtf.js
@@ -2,8 +2,9 @@
 
   'use strict';
 
-  var forbiddenWords = ['javascript:void(0);', 'javascript:void(0)', 'javascript:', '#'];
+  var forbiddenWords = ['javascript:void(0);', '#', 'javascript:void(0)', 'javascript:'];
   var anchors = document.querySelectorAll('a'),
+  // var as = Array.prototype.slice.call(document.querySelectorAll('a')),
       len,
       x, z,
       e, i;
@@ -11,6 +12,15 @@
   function stopThat(evt) {
     evt.preventDefault();
   }
+
+  // as.forEach(function() {
+  //   for (x = 0; x < forbiddenWords.length; x++) {
+  //     if (as[i] == forbiddenWords[x]) {
+  //       as[i].href = '';
+  //       as[i].addEventListener(click, stopThat, false);
+  //     }
+  //   }
+  // });
 
   for (i = 0; i < anchors.length; i++) {
     for (x = 0; x < forbiddenWords.length; x++) {
