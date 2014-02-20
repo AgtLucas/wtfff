bdb7b3cce5dd4a1a78a3a93b47e34eb05a5e1db8
Lucas <AgtLucas@users.noreply.github.com>
2014-02-20 12:52:52 -0300

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -78,11 +78,6 @@ module.exports = function(grunt) {
 
   });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-connect');
-
   grunt.registerTask('default', ['connect', 'watch']);
 
 }
